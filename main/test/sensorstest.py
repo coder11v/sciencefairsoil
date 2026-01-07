d4eb7215d78a8fe9ae67cb8c81439510633173c4
vib@thecookie <veerbajaj11@gmail.com>
2026-01-06 21:23:30 -0800

    Runtime error caching

--- main/test/sensorstest.py
+++ main/test/sensorstest.py
@@ -1,6 +1,12 @@
-from sensors import get_moisture
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+
 from sensors import *
 
 if __name__ == "__main__":
-    get_moisture_raw(a1)
-    get_moisture(a1)
+    raw = get_moisture_raw("a1")
+    per = get_moisture("a1")
+    print(f"\nRaw: {raw}\nPercentage: {per}\n")
